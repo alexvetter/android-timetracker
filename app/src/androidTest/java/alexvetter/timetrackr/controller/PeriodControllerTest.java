16388f584d6a3470ebd59d84b4bdb1ce7c96b88f
Alexander Vetter <vettera@gmail.com>
2015-06-24 11:58:17 +0200

    add androidTest for working hours calculation

--- app/src/androidTest/java/alexvetter/timetrackr/controller/PeriodControllerTest.java
+++ app/src/androidTest/java/alexvetter/timetrackr/controller/PeriodControllerTest.java
@@ -6,6 +6,7 @@ import android.test.RenamingDelegatingContext;
 import net.danlew.android.joda.JodaTimeAndroid;
 
 import org.joda.time.DateTime;
+import org.joda.time.Duration;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,11 +14,14 @@ import java.util.UUID;
 import alexvetter.timetrackr.database.SQLiteHelper;
 import alexvetter.timetrackr.domain.BeaconModel;
 import alexvetter.timetrackr.domain.PeriodModel;
+import alexvetter.timetrackr.utils.DateTimeFormats;
+import alexvetter.timetrackr.utils.TargetHours;
 
 public class PeriodControllerTest extends AndroidTestCase {
 
     private RenamingDelegatingContext context;
     private PeriodController controller;
+    private TargetHours targetHoursSettings;
 
     @Override
     public void setUp() throws Exception {
@@ -29,7 +33,7 @@ public class PeriodControllerTest extends AndroidTestCase {
         SQLiteHelper.setInstance(context);
 
         controller = new PeriodController(context);
-
+        targetHoursSettings = new TargetHours(context);
     }
 
     @Override
@@ -102,7 +106,38 @@ public class PeriodControllerTest extends AndroidTestCase {
     }
 
     public void testCalculateTotalDuration() throws Exception {
+        // Mi, 24. Juni 2015
+        DateTime initial = DateTime.parse("2015-06-24 08:00", DateTimeFormats.DATE_TIME);
 
+        // add period1
+        PeriodModel period1 = controller.getDefaultPeriodModel();
+        period1.setStartTime(initial);
+        period1.setEndTime(initial.plusHours(TargetHours.DEFAULT_HOURS_WORKDAY));
+
+        controller.addOrUpdate(period1);
+
+        // add period2
+        PeriodModel period2 = controller.getDefaultPeriodModel();
+        period2.setStartTime(initial.plusDays(1));
+        period2.setEndTime(initial.plusDays(1).plusHours(TargetHours.DEFAULT_HOURS_WORKDAY));
+
+        controller.addOrUpdate(period2);
+
+        Duration totalTarget = new Duration(0);
+
+        Duration target1 = targetHoursSettings.getDuration(period1.getStartTime());
+        Duration target2 = targetHoursSettings.getDuration(period2.getStartTime());
+
+        totalTarget = totalTarget.plus(target1).plus(target2);
+
+        Duration totalWork = new Duration(0);
+
+        Duration duration1 = new Duration(period1.getStartTime(), period1.getEndTime());
+        Duration duration2 = new Duration(period2.getStartTime(), period2.getEndTime());
+
+        totalWork = totalWork.plus(duration1).plus(duration2);
+
+        assertEquals(totalWork.toPeriod().minus(totalTarget.toPeriod()), controller.calculateTotalDuration());
     }
 
     public void testAddOrUpdate() throws Exception {
